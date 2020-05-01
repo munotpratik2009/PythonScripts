7f46b9898660082c8be3326041c3f3d61732a667
Pratik Munot <munotpratik2009@gmail.com>
2020-05-01 13:40:31 +0530

    Update pdfCracker.py

--- pdfCracker.py
+++ pdfCracker.py
@@ -1,7 +1,16 @@
+"""
+A basic attempt to PDF cracking. 
+I had a pdf which was password protected and I had lost the password. 
+I partially remembered the password and I wanted to write a program 
+which will generate the password based on this partial password. 
+And it did work! I was able to open the file with this password
+generator
+"""
+
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 def decryptPdf(f):
-        for ps in range(3000000, 9999999):
+	for ps in range(3000000, 9999999):
 		print('Current Run:' + str(ps))
 		if(f.getIsEncrypted()):
 			t = f.decrypt(str(ps) + '7257')
